4b06c5ce780de8b06d1e3d3c77956296dadedb46
Suraj Kumar <sahsuraj2001@gmail.com>
2023-04-18 15:48:38 +0530

    change file

--- src/script.js
+++
@@ -1,9 +0,0 @@
-
-document.querySelector(".fa-bars").addEventListener('click', () => {
-    document.querySelector(".item").classList.add("show")
-})
-
-document.querySelector(".fa-xmark").addEventListener("click", () => {
-    document.querySelector(".item").classList.remove("show")
-})
-
